9c9c7fcf3063ac73f273490e2ebafc48693dfe0e
mboger <marko.boger@gmail.com>
2019-12-13 20:19:28 +0100

    added vue

---
+++ dashboard.js
@@ -0,0 +1,10 @@
+var dashboard = new Vue({
+    el:'#dashboard',
+    data:{
+        time:"12:34:56 <br>  00:45:20",
+        item1:'1',
+        item2:'2',
+        item3:'3',
+        item4:'4'
+    }
+})
