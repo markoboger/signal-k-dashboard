fd894014e7406088be895df7aef038bb61627447
mboger <marko.boger@gmail.com>
2019-12-15 00:30:09 +0100

    added pwa and changed grid layout to 10*26 for landscape and 8*30 for portrait

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,20 @@
 var dashboard = new Vue({
-    el:'#dashboard',
+    el:"#dashboard",
     data:{
-        time:"12:34:56 <br>  00:45:20",
-        item1:'1',
+        time:"12:36:56",
+        countdown:"00:45:20",
+        item1:'12,34',
         item2:'2',
         item3:'3',
         item4:'4'
     }
 })
+
+Vue.component('sk-data-box', {
+    data: function () {
+      return {
+        data: 0
+      }
+    },
+    template: '<div>{{data}}</div>'
+  })
