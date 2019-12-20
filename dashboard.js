cb75c5b20d1c1a77f64bc4e21746de011456d55e
mboger <marko.boger@htwg-konstanz.de>
2019-12-20 13:09:47 +0100

    started rotation of outer rose, works with inplace values but not with params.

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,4 @@
+
 function addOrResizeCanvas(containerId, canvasId, newWidth, newHeight) {
     let container = document.getElementById(containerId);
     let oldCanvas = document.getElementById(canvasId);
@@ -17,7 +18,7 @@ function textplacing(radius, length, fontsize, direction) {
     else if (direction == -1) return -radius-(length+fontsize/2)*direction
 }
 
-function drawCicularScale(canvas, x, y, radius, direction) {
+function drawCircularScale(canvas, x, y, radius, direction, angle) {
     let context = canvas.getContext("2d");
     context.strokestyle = "black";
     let fontsize = fontsizeForRadius(radius);
@@ -28,7 +29,12 @@ function drawCicularScale(canvas, x, y, radius, direction) {
     context.stroke();
     for(var i = 0; i < 360; i+=10) {
         var length = fontsize/4;
-        if (i%30 ==0) { 
+        if (i%90 == 0) {
+            context.font =  (fontsize*2).toString +'px '+this.fontFamily;
+        } else {
+            context.font =  (fontsize).toString +'px '+this.fontFamily;
+        }
+        if (i%30 == 0) { 
             length =length*2;
             context.fillText(i.toString(),((-fontsize/6)*digits(i)),textplacing(radius, length, fontsize, direction));
         }
@@ -37,20 +43,20 @@ function drawCicularScale(canvas, x, y, radius, direction) {
         context.stroke();
         context.rotate(Math.PI/18);
     }
+    context.rotate(angle);
     context.translate(-x,-y);
 }
 
-function drawRose() {
+function drawRose(innerAngle, outerAngle) {
     let width =  $("#dashboard").width();
     let height =  $("#dashboard").height();
     let boatroseCanvas = addOrResizeCanvas("rose", "boatrose", width, height);
     let compassroseCanvas = addOrResizeCanvas("rose", "compassrose", width, height);
 
     let radius=Math.min(width, height)/2;
-    drawCicularScale(compassroseCanvas, width/2,height/2, radius*0.8, -1);
-    drawCicularScale(boatroseCanvas, width/2,height/2, radius*0.7, 1);
+    drawCircularScale(compassroseCanvas, width/2,height/2, radius*0.8, -1, outerAngle);
+    drawCircularScale(boatroseCanvas, width/2,height/2, radius*0.7, 1, innerAngle);
     let context = boatroseCanvas.getContext("2d");
-    context.drawImage(boatroseCanvas, 0, 0, width, height);
     context.drawImage(compassroseCanvas, 0, 0, width, height);
 }
 
@@ -66,13 +72,16 @@ function swapoutIdSubstring(oldStr, newStr) {
         }  
 }
 
+let hdg = Math.PI;
+function degToRad(degrees) { return degrees * (Math.PI/180); }
+
 function doOnOrientationChange() {
     if(landscape()){
         swapoutIdSubstring("portrait","landscape");
     } else {
         swapoutIdSubstring("landscape","portrait");
     }
-    drawRose();
+    drawRose(0,hdg);
 }
 let oldOrientation = true;
 let newOrientation = true;
@@ -82,7 +91,7 @@ function checkOrientationChange() {
     if (newOrientation != oldOrientation) {
         doOnOrientationChange();
     } else {
-        drawRose();
+        drawRose(0, hdg);
     }
     oldOrientation=newOrientation;
 }
@@ -111,47 +120,49 @@ Vue.component('data-box', {
         </div>`
     })
 
-  var dashboard = new Vue({
-    el:"#dashboard",
-    data:{
-        lat:'50° 34,56',
-        lon:'009° 56,89',
-        time:"12:36:56",
-        countdown:"00:45:20",
-        rpm:"2700 RPM",
-        log:"12,345 nm",
-        heading:'360',
-        depth:'18,5',
-        stw:'12,34',
-        sog:'8,90',
-        aws:'15,89',
-        tws:'10,40',
-        vmg:'3,40',
-        target:'358',
-        perf:'96,7',
-        temp:'21,3',
-        water_temp:'19,0',
-        fuel:'89',
-        fuel2:'0',
-        bat:'98',
-        bat2:'100',
-        stw_avg:'7,89',
-        sog_avg:'8,76',
-        tws_avg:'11,90',
-        aws_avg:'10,99',
-        stw_max:'7,89',
-        sog_max:'8,76',
-        tws_max:'11,90',
-        aws_max:'10,99',
-        vmg_avg:'3,78',
-        vmg_max:'3,65',
-    }
-})
 
+    var dashboard = new Vue({
+        el:"#dashboard",
+        data:{
+            lat:'50° 34,56',
+            lon:'009° 56,89',
+            time:"12:36:56",
+            countdown:"00:45:20",
+            rpm:"2700 RPM",
+            log:"12,345 nm",
+            heading:'360',
+            depth:'18,5',
+            stw:'12,34',
+            sog:'8,90',
+            hdg:'240',
+            aws:'15,89',
+            tws:'10,40',
+            vmg:'3,40',
+            target:'358',
+            perf:'96,7',
+            temp:'21,3',
+            water_temp:'19,0',
+            fuel:'89',
+            fuel2:'0',
+            bat:'98',
+            bat2:'100',
+            stw_avg:'7,89',
+            sog_avg:'8,76',
+            tws_avg:'11,90',
+            aws_avg:'10,99',
+            stw_max:'7,89',
+            sog_max:'8,76',
+            tws_max:'11,90',
+            aws_max:'10,99',
+            vmg_avg:'3,78',
+            vmg_max:'3,65',
+        }
+    })
+    
 $('document').ready(function(){
     window.addEventListener('orientationchange', doOnOrientationChange);
     window.addEventListener('resize', checkOrientationChange);
-    drawRose();
+    drawRose(0, hdg);
     oldOrientation = true;
     newOrientation = true;
     checkOrientationChange();
